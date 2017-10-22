214c287d6b12436d44def253c16782b6fc32f56e
arshad <arshad2117@gmail.com>
2017-10-23 00:50:44 +0530

    Added breadth functionality

--- crawl.py
+++ crawl.py
@@ -2,6 +2,7 @@ import urllib.request, urllib.parse, urllib.error
 import ssl
 from bs4 import BeautifulSoup
 import os
+import queue
 
 #ignore ssl certification errors
 ctx=ssl.create_default_context()
@@ -12,7 +13,7 @@ headers={'User-agent' : "Mozilla/5.0"}
 
 found={}
 
-def crawl(url):
+def crawldfs(url):
 
 	if(url == None or ("https://en.wikipedia.org"+url) in found ):
 		return
@@ -44,11 +45,59 @@ def crawl(url):
 
 	for link in anchor:
 
-		crawl(link.get('href',None))
+		crawldfs(link.get('href',None))
+
+
+def crawlbfs(urlbegin):
+
+	q=queue.Queue()
+	
+	q.put(urlbegin)
+
+	while(not q.empty()):
+		
+		url=q.get()
+
+		if(url == None or ("https://en.wikipedia.org"+url) in found ):
+			continue
+
+		if(url.find("/wiki/") != 0 or url.find("(") != -1 or url.find(":") != -1 ):
+			continue
+
+
+		try:
+			url="https://en.wikipedia.org"+url
+			
+			req=urllib.request.Request(url,None,headers)
+
+			page=urllib.request.urlopen(req)
+
+			cont=page.read()
+
+		except:
+
+			continue
+
+		found[url]=1;
+
+		print(url)
+
+		bsoup = BeautifulSoup(cont,"html.parser")
+
+		anchor = bsoup('a')
+
+		for link in anchor:
+
+			q.put(link.get('href',None))
 
 
 getfirst = input("ENTER STARTING URL: ")
 
+x=input("Breadth or depth(0/1): ")
+
 starter = getfirst[24:]
 
-crawl(starter)
+if(int(x)==1):
+	crawldfs(starter)
+else:
+	crawlbfs(starter)
